1f9387460833374fec9792f1cd7e85cc20f760b1
Aidan Winters <aidan@norgay>
2023-11-28 20:16:06 -0800

    updated perturbation annotation matrix

--- processing.py
+++ processing.py
@@ -2,7 +2,6 @@ from scipy.sparse import csr_matrix
 import numpy as np
 import pandas as pd
 
-
 def filter_to_feature_type(
     adata,
     feature_type='Gene Expression'
@@ -35,15 +34,17 @@ def generate_perturbation_matrix(
     perturbation_col = 'feature_call',
     delim = '|',
     feature_list = None,
-    sparse = True,
+    # sparse = True,
+    verbose = True,
     ):
-    print(adata)
 
     #if there is no feature list, split all the features in the column and build one
     if feature_list is None:
         #get all the features but not nan
         labels = adata.obs[perturbation_col].dropna()
         feature_list = labels.str.split(delim).explode().unique()
+        if verbose:
+            print(f"Found {len(feature_list)} unique features.")
 
     #create a matrix of zeros with the shape of the number of cells and number of features
     perturbation_matrix = np.zeros((adata.shape[0], len(feature_list)))
@@ -69,10 +70,34 @@ def generate_perturbation_matrix(
 
     #print num null 
 
-    if sparse:
-        return csr_matrix(perturbation_matrix)
+    # if sparse:
+    #     return csr_matrix(perturbation_matrix)
     else: 
         return pd.DataFrame(perturbation_matrix, index=adata.obs.index, columns=feature_list)
+
+def add_perturbation_matrix(adata):
+    """
+    Add a perturbation matrix to an anndata object. 
+    """
+
+    adata.obsm['perturbation'] = pm.loc[guides.obs.index, :].values
+    guides.uns['perturbation_var'] = pm.columns.tolist()
+    guides
+
+    if inplace:
+        adata.layers['perturbations'] = generate_perturbation_matrix(
+            adata,
+            perturbation_col = perturbation_col,
+            feature_list = feature_list,
+            inplace = inplace
+            )
+    else:
+        return generate_perturbation_matrix(
+            adata,
+            perturbation_col = perturbation_col,
+            feature_list = feature_list,
+            inplace = inplace
+            )
 
 def split_sort_trim(label, delim='|', delim2='_'):
     #if not string then print
