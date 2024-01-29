159e4fae00ec92f17558f83db2467b9beab97e9f
Aidan Winters <aidanwinters95@gmail.com>
2024-01-29 11:12:05 -0800

    updated README with vague install instructions, changed python version requirement."

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # pyturbseq
 
+Make sure that you have an environment with python >=3.8 and <3.12
+Recommended install into conda/mamba environment: 
+```
+conda create -n pyturbseq python=3.11
+conda activate pyturbseq
+```
 
 To install: 
 ```
-git clone https://github.com/aidanwinters/pyturbseq
-cd pyturbseq
-pip install .
+pip install pyturbseq
 ```
--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
+
+# Read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='pyturbseq',
     author='Aidan Winters',
     author_email='aidanfwinters@gmail.com',
     version='0.0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    python_requires='>3.11',
+    python_requires='>=3.8,<3.12',
     install_requires=[
         'scanpy',
         'scipy',
