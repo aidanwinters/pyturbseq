49e5f1f120cb991749206fd68f7c7d10c610a9a2
Aidan Winters <aidanwinters95@gmail.com>
2024-02-06 21:27:59 -0800

    close #1; moved to 0.0.4 to push and fix target change function.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='pyturbseq',
     author='Aidan Winters',
     author_email='aidanfwinters@gmail.com',
-    version='0.0.3',
+    version='0.0.4',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=['pyturbseq'],
